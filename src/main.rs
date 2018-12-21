cf7cce8d7b19534194bd7f22d544f074c9128f4c
Zhenyuan Lau <zhenyuanlau@icloud.com>
2018-12-21 08:23:15 +0800

    exception processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    // panic!("crash and burn");
+
+    // let v = vec![1, 2, 3];
+
+    // v[99];
+
+    let f = File::open("hello.txt");
+
+    // let f: u32 = File::open("hello.txt");
+
+    match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
